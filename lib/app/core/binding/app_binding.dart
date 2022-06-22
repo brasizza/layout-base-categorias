6fe00ef530e2c022de01b67a5c7a938227accd63
Marcus Brasizza <mvbdesenvolvimento@gmail.com>
2022-06-22 17:36:54 -0300

    remove app binding

--- lib/app/core/binding/app_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:cardapio/app/core/hive/hive_init.dart';
-import 'package:get/instance_manager.dart';
-
-class AppBinding extends Bindings {
-  @override
-  void dependencies() async {
-    Get.put(await HiveInit.init());
-  }
-}
